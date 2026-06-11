3dc0ea467836724d2a50c44d13c95e011e2cf698
wkkanekin <wkkanekin@gmail.com>
2026-06-11 16:36:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1608,5 +1608,25 @@ setHitLabel(`全学生：${enabledStudents.length}名`);
  console.error(e);
  if (mapStatusEl) mapStatusEl.textContent = "地図の初期化でエラー";
  }
+
+// ======================
+// 問い合わせフォーム蛇腹
+// ======================
+
+const contactAccordionBtn =
+  document.getElementById("contactAccordionBtn");
+
+const contactAccordionBody =
+  document.getElementById("contactAccordionBody");
+
+if (
+  contactAccordionBtn &&
+  contactAccordionBody
+) {
+  contactAccordionBtn.addEventListener("click", () => {
+    contactAccordionBody.classList.toggle("open");
+  });
+}
+
  })();
 });

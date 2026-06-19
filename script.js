9c182795079d0d651342fbe21a9ce8ee1c0ccc30
wkkanekin <wkkanekin@gmail.com>
2026-06-19 19:15:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -896,28 +896,10 @@ async function tryLoadImagesAny() {
  function applyImages(cfg) {
  if (!cfg || typeof cfg !== "object") return;
 
-let heroUrl = "";
-
-if (window.innerWidth <= 768) {
-  heroUrl = getVersionedImageUrl(
-    cfg?.hero,
-    ["mobileImageUrl", "imageUrl", "url"],
-    ["mobileImageVersion", "imageVersion", "version", "v"]
-  );
-} else {
-  heroUrl = getVersionedImageUrl(
-    cfg?.hero,
-    ["pcImageUrl", "imageUrl", "url"],
-    ["pcImageVersion", "imageVersion", "version", "v"]
-  );
-}
-
-if (heroUrl) {
-  document.documentElement.style.setProperty(
-    "--hero-image",
-    `url("${heroUrl}")`
-  );
-}
+ const heroUrl = getVersionedImageUrl(cfg?.hero, ["imageUrl", "url"], ["imageVersion", "version", "v"]);
+ if (heroUrl) {
+ document.documentElement.style.setProperty("--hero-image", `url("${heroUrl}")`);
+ }
 
  const logoUrl = getVersionedImageUrl(
  cfg?.hero,
